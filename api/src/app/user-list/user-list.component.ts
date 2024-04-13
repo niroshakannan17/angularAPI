46bc09fe7af67f830ef64c888d94ef41901991e9
nirosha <niroshakannan17@gmail.com>
2024-04-13 12:32:31 +0530

    get api example added

--- api/src/app/user-list/user-list.component.ts
+++ api/src/app/user-list/user-list.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; // class file
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit{
 
+  user:User[]=[];
+
+  constructor(private http:HttpClient){
+
+  }
+
+  // when we have to load data on page load
+  //so we have to use ngoninit 
+  ngOnInit(): void {
+    
+    this.getUserData().subscribe((response)=>{
+      console.log('userData :',response);
+      this.user = response;
+    })
+  }
+
+  getUserData(){
+    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users')
+  }
+
+}
+
+class User{
+  name!:string;
 }
