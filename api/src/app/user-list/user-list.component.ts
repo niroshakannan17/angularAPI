dc3dae8d9cda8312595ba3b8a9ed355c6533df24
nirosha <niroshakannan17@gmail.com>
2024-04-13 13:18:24 +0530

    post api called example

--- api/src/app/user-list/user-list.component.ts
+++ api/src/app/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; // class file
 import { OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-list',
@@ -29,8 +30,31 @@ export class UserListComponent implements OnInit{
     return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users')
   }
 
+  
+
+
+  userAddform = new FormGroup({
+    username : new FormControl(),
+    useremail : new FormControl()
+  })
+
+  addUserData(){
+    return this.http.post<User>('https://jsonplaceholder.typicode.com/users',{
+      name : this.userAddform.controls.username.value,
+      email :  this.userAddform.controls.useremail.value
+     })
+  }
+
+  save(){
+    console.log(this.userAddform)
+    this.addUserData().subscribe((response)=>{
+      this.user.push(response)
+    })
+  }
+
 }
 
 class User{
   name!:string;
+  email!:string;
 }
